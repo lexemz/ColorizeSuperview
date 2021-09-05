05f52038b381af59656f1f5370823ce693dafd0c
Igor Buzykin <lexemz@gmail.com>
2021-09-06 00:32:22 +0300

    Colorize VC outlets update

--- ColorizeView/ViewControllers/ColorizeViewController.swift
+++ ColorizeView/ViewControllers/ColorizeViewController.swift
@@ -7,27 +7,23 @@
 
 import UIKit
 
-
 protocol SettingsViewControllerDelegate {
     func setNewUIColor(for view: UIColor)
 }
 
 class ColorizeViewController: UIViewController {
-    
-    @IBOutlet var superView: UIView!
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let settingsVC = segue.destination as? SettingsViewController else {
             return
         }
 
-        settingsVC.colorizeVCSuperViewColor = superView.backgroundColor
+        settingsVC.colorizeVCSuperViewColor = view.backgroundColor
         settingsVC.delegate = self
     }
 }
 
 extension ColorizeViewController: SettingsViewControllerDelegate {
     func setNewUIColor(for view: UIColor) {
-        superView.backgroundColor = view
+        self.view.backgroundColor = view
     }
 }
